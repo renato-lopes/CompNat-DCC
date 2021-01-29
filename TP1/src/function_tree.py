8bcd454f0dd2122e25bc595f774c09bf245cb7ad
Renato <renato.junior@dcc.ufmg.br>
2021-01-29 13:07:13 -0300

    Implement crossover

--- TP1/src/function_tree.py
+++ TP1/src/function_tree.py
@@ -4,14 +4,26 @@ FUNCTION_NODE=0 # Node with a function, like sum and mul
 CONSTANT_NODE=1 # Node with a constant, like a real number
 VAR_NODE=2 # Node with a reference to a variable (based on variables array index)
 
+LEFT_CHILD=0
+RIGHT_CHILD=1
+
 class Tree:
-    def __init__(self, node_type, node_value, lchild=None, rchild=None):
+    def __init__(self, node_type, node_value, parent=None, lchild=None, rchild=None):
         self.node_type = node_type
         self.node_value = node_value
 
+        self.parent = parent
         self.lchild = lchild
         self.rchild = rchild
     
+    def set_left_child(self, node):
+        self.lchild = node
+        node.parent = (self, LEFT_CHILD)
+
+    def set_right_child(self, node):
+        self.rchild = node
+        node.parent = (self, RIGHT_CHILD)
+
     def evaluate(self, variables):
         if self.node_type == CONSTANT_NODE:
             return self.node_value
@@ -30,7 +42,7 @@ class Tree:
         else:
             return max(1+self.lchild.height(), 1+self.rchild.height())
 
-    def tostring(self, level):
+    def tostring(self, level=0):
         rep = '\t'*level
         if self.node_type == FUNCTION_NODE:
             rep += str(self.node_value)+"\n"
@@ -42,3 +54,16 @@ class Tree:
             rep += str(self.node_value)+"\n"
         return rep
 
+    def get_nodes_at_level(self, nodes, level):
+        if level == 0:
+            nodes.append(self)
+        elif self.node_type == FUNCTION_NODE:
+            self.lchild.get_nodes_at_level(nodes, level-1)
+            self.rchild.get_nodes_at_level(nodes, level-1)
+
+    def copy(self):
+        cp = Tree(self.node_type, self.node_value)
+        if self.node_type == FUNCTION_NODE:
+            cp.set_left_child(self.lchild.copy())
+            cp.set_right_child(self.rchild.copy())
+        return cp
--- TP1/src/genetics.py
+++ TP1/src/genetics.py
@@ -1,13 +1,13 @@
 import random
 
-from function_tree import FUNCTION_NODE, CONSTANT_NODE, VAR_NODE, Tree
+from function_tree import FUNCTION_NODE, CONSTANT_NODE, VAR_NODE, LEFT_CHILD, RIGHT_CHILD, Tree
 from symbols import get_random_function, get_random_constant, get_random_variable
 
 def init_tree_full(height, num_variables, p_constant=0.5):
     if height > 0:
         root = Tree(FUNCTION_NODE, get_random_function()) # Internal nodes are only function nodes
-        root.lchild = init_tree_full(height-1, num_variables, p_constant)
-        root.rchild = init_tree_full(height-1, num_variables, p_constant)
+        root.set_left_child(init_tree_full(height-1, num_variables, p_constant))
+        root.set_right_child(init_tree_full(height-1, num_variables, p_constant))
     else:
         if random.random() < p_constant: # Choose between constant terminal or variable terminal
             root = Tree(CONSTANT_NODE, get_random_constant())
@@ -15,12 +15,12 @@ def init_tree_full(height, num_variables, p_constant=0.5):
             root = Tree(VAR_NODE, get_random_variable(num_variables))
     return root
 
-def init_tree_grow(height, num_variables, p_function=0.5, p_constant=0.5):
+def init_tree_grow(height, num_variables, p_function=0.7, p_constant=0.2):
     if height > 0:
         if random.random() < p_function: # Choose between function or terminal
             root = Tree(FUNCTION_NODE, get_random_function())
-            root.lchild = init_tree_grow(height-1, num_variables)
-            root.rchild = init_tree_grow(height-1, num_variables)
+            root.set_left_child(init_tree_grow(height-1, num_variables))
+            root.set_right_child(init_tree_grow(height-1, num_variables))
         elif random.random() < p_constant: # Choose between constant terminal or variable terminal
             root = Tree(CONSTANT_NODE, get_random_constant())
         else:
@@ -51,3 +51,33 @@ def initialize_population(population_size, tree_height, num_variables):
             population.append(tree)
 
     return population
+
+def crossover(t1, t2):
+    t1 = t1.copy()
+    t2 = t2.copy()
+    height = min(t1.height(), t2.height())
+    crossover_level = random.randrange(height+1)
+    # Get all nodes in the chosen level in both trees
+    nodes_t1 = []
+    t1.get_nodes_at_level(nodes_t1, crossover_level)
+    nodes_t2 = []
+    t2.get_nodes_at_level(nodes_t2, crossover_level)
+    # Choose random node in each tree at the chosen level
+    n_t1 = nodes_t1[random.randrange(len(nodes_t1))]
+    n_t2 = nodes_t2[random.randrange(len(nodes_t2))]
+    # Swap nodes between trees
+    p_t1 = n_t1.parent
+    p_t2 = n_t2.parent
+    n_t1.parent = p_t2
+    n_t2.parent = p_t1
+    if p_t2 is not None:
+        if p_t2[1] == LEFT_CHILD:
+            p_t2[0].lchild = n_t1
+        else:
+            p_t2[0].rchild = n_t1
+    if p_t1 is not None:
+        if p_t1[1] == LEFT_CHILD:
+            p_t1[0].lchild = n_t2
+        else:
+            p_t1[0].rchild = n_t2
+    return t1, t2
